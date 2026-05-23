05785fd804417502024a961ed1698212f6ff68ec
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2026-05-23 20:02:12 +0530

    Update sql_queries_day1.sql

--- Week1/Day1/sql_queries_day1.sql
+++ Week1/Day1/sql_queries_day1.sql
@@ -102,3 +102,30 @@ WHERE name LIKE '_________';
 SELECT *
 FROM Employee
 WHERE name LIKE '_o%';
+
+-- Problem 3: Date Queries
+
+-- Q11.Select employees hired in the year 2020
+SELECT *
+FROM Employee
+WHERE YEAR(hire_date) = 2020;
+
+-- Q12.Select employees hired in January of any year
+SELECT *
+FROM Employee
+WHERE MONTH(hire_date) = 1;
+
+-- Q13.Select employees hired before 2019
+SELECT *
+FROM Employee
+WHERE YEAR(hire_date) < 2019;
+
+-- Q14.Select employees hired on or after March 1, 2021
+SELECT *
+FROM Employee
+WHERE hire_date >= '2021-03-01';
+
+-- Q15.Select employees hired in the last 2 years
+SELECT *
+FROM Employee
+WHERE hire_date >= CURRENT_DATE - INTERVAL 2 YEARS;
