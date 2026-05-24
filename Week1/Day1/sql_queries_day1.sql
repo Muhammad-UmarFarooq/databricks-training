08f784a575b6a89addd3ff580d15325ad6e077b8
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2026-05-24 16:25:50 +0530

    Add aggregate queries for employee salaries and counts

--- Week1/Day1/sql_queries_day1.sql
+++ Week1/Day1/sql_queries_day1.sql
@@ -129,3 +129,27 @@ WHERE hire_date >= '2021-03-01';
 SELECT *
 FROM Employee
 WHERE hire_date >= CURRENT_DATE - INTERVAL 2 YEAR;
+
+-- Problem 4: Aggregate Queries
+
+-- Q16.Select the total salary of all employees
+SELECT SUM(salary) AS total_salary
+FROM Employee;
+
+-- Q17.Select the average salary of employees
+SELECT AVG(salary) AS average_salary
+FROM Employee;
+
+-- Q18.Select the minimum salary in the Employee table
+SELECT MIN(salary) AS minimum_salary
+FROM Employee;
+
+-- Q19.Select the number of employees in each department
+SELECT department_id, COUNT(*) AS employee_count
+FROM Employee
+GROUP BY department_id;
+
+-- Q20.Select the average salary of employees in each department
+SELECT department_id, AVG(salary) AS average_salary
+FROM Employee
+GROUP BY department_id;
