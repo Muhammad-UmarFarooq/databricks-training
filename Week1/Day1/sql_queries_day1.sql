5c1fd8a5fdc37ffb9e8137daa085cfe024e86af0
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2026-06-03 14:55:49 +0530

    Update sql_queries_day1.sql

--- Week1/Day1/sql_queries_day1.sql
+++ Week1/Day1/sql_queries_day1.sql
@@ -182,3 +182,33 @@ FROM Employee
 GROUP BY department_id
 ORDER BY average_salary DESC
 LIMIT 1;
+
+-- Q26.Select departments with more than 2 employees
+SELECT department_id, COUNT(*) AS employee_count
+FROM Employee
+GROUP BY department_id
+HAVING COUNT(*) > 2;
+
+-- Q27.Select departments with an average salary greater than 55000
+SELECT department_id, AVG(salary) AS average_salary
+FROM Employee
+GROUP BY department_id
+HAVING AVG(salary) > 55000;
+
+-- Q28.Select years with more than 1 employee hired
+SELECT YEAR(hire_date) AS hire_year, COUNT(*) AS employee_count
+FROM Employee
+GROUP BY YEAR(hire_date)
+HAVING COUNT(*) > 1;
+
+-- Q29.Select departments with a total salary expense less than 100000
+SELECT department_id, SUM(salary) AS total_salary
+FROM Employee
+GROUP BY department_id
+HAVING SUM(salary) < 100000;
+
+-- Q30.Select departments with the maximum salary above 75000
+SELECT department_id, MAX(salary) AS maximum_salary
+FROM Employee
+GROUP BY department_id
+HAVING MAX(salary) > 75000;
